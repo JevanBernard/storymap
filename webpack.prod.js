e5e6c2ee444f5e46d042537ae92f5213d200dbc0
JevanBernard <jevan.bernard1007@gmail.com>
2025-11-13 13:13:06 +0800

    hapus komentar

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,6 @@ const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-// Use relative publicPath so the build works both on local server and GitHub Pages
 const publicPath = './';
 
 module.exports = merge(common, {
